7995ae32152d18676d93d37566284e7f85d83326
Kristers Buivids <A250804KB@rvt.lv>
2026-05-27 16:16:15 +0300

    Jurija clicker

--- script.js
+++ script.js
@@ -1,32 +1,90 @@
-let cookies = 0;
-let upgrades = 0;
-let cost = 50;
+let score = 0;
+let cps = 0;
 
+const scoreEl = document.getElementById("score");
+const cpsEl = document.getElementById("cps");
 const cookie = document.getElementById("cookie");
-const counter = document.getElementById("counter");
-const upgradeBtn = document.getElementById("upgrade");
-const costText = document.getElementById("cost");
-const upgradeCount = document.getElementById("upgrades");
+const shop = document.getElementById("shop");
+
+const upgrades = [
+    {
+        name: "Crimping Tool",
+        cost: 50,
+        power: 1
+    },
+    {
+        name: "RJ-45 Connectors",
+        cost: 200,
+        power: 5
+    },
+    {
+        name: "Jack",
+        cost: 1000,
+        power: 20
+    },
+    {
+        name: "Optiskā Šķiedra",
+        cost: 5000,
+        power: 100
+    },
+    {
+        name: "Switch",
+        cost: 20000,
+        power: 500
+    },
+    {
+        name: "Server Rack",
+        cost: 100000,
+        power: 2000
+    }
+];
 
 cookie.addEventListener("click", () => {
-    cookies += 1 + upgrades;
-    counter.textContent = cookies;
+    score++;
+    updateUI();
 });
 
-upgradeBtn.addEventListener("click", () => {
-    if (cookies >= cost) {
-        cookies -= cost;
-        upgrades++;
-        cost = Math.floor(cost * 1.25);
+function updateUI() {
+    scoreEl.innerText = Math.floor(score) + " Bald Coins";
+    cpsEl.innerText = cps + " per second";
+}
 
-        counter.textContent = cookies;
-        upgradeCount.textContent = upgrades;
-        costText.textContent = cost;
-    }
-});
+function createShop() {
+    upgrades.forEach(upgrade => {
+        const item = document.createElement("div");
+        item.className = "shop-item";
 
-// Automātiska sīkdatēs ģenerēšana
+        item.innerHTML = `
+            <h3>${upgrade.name}</h3>
+            <p>Cost: ${upgrade.cost}</p>
+            <p>+${upgrade.power} cps</p>
+        `;
+
+        item.addEventListener("click", () => {
+            if(score >= upgrade.cost) {
+                score -= upgrade.cost;
+                cps += upgrade.power;
+
+                upgrade.cost = Math.floor(upgrade.cost * 1.5);
+
+                item.innerHTML = `
+                    <h3>${upgrade.name}</h3>
+                    <p>Cost: ${upgrade.cost}</p>
+                    <p>+${upgrade.power} cps</p>
+                `;
+
+                updateUI();
+            }
+        });
+
+        shop.appendChild(item);
+    });
+}
+
 setInterval(() => {
-    cookies += upgrades;
-    counter.textContent = cookies;
+    score += cps;
+    updateUI();
 }, 1000);
+
+createShop();
+updateUI();
